0ed31e2d7abf6ee48ae503369a34e689e2341156
Karamjeet singh <149044321+mysticai11@users.noreply.github.com>
2024-01-14 21:59:06 +0530

    Add files via upload

---
+++ deepthought.py
@@ -0,0 +1,7 @@
+ask = (input("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
+if (ask==42,"forty-two","forty two"):
+  print("Yes")
+elif (ask==50,"fifty","Fifty"):
+  print("No")
+else:
+  print("No")
