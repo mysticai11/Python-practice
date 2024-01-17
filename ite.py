4e0ff7a0fe32cfc2b8d09defd128928c68a06f59
Karamjeet singh <149044321+mysticai11@users.noreply.github.com>
2024-01-17 19:13:16 +0530

    Add files via upload

---
+++ ite.py
@@ -0,0 +1,5 @@
+my_list = [12, 34, 56, 77, 85, 33]
+total = 0
+for i in range(0, len(my_list)):
+    total = total + my_list[i]
+print(total)
---
+++ iterate_by_value.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3, "anii", "cd", -99, 99.88]
+
+# iterate by value
+for i in a:
+    print(i, end=",")
