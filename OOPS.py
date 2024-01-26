f7befbcfba53dc5b3df5f2507d9dfa5c9944447c
Karamjeet singh <149044321+mysticai11@users.noreply.github.com>
2024-01-26 16:30:48 +0530

    Create OOPS.py

---
+++ OOPS.py
@@ -0,0 +1,22 @@
+def oops_program():
+    print("This is an Oops program.")
+    
+    # Error: Missing colon at the end of the if statement
+    if True
+        print("Oops, there's an error!")
+
+    # Error: Indentation is incorrect
+    print("Indentation error!")
+
+    # Error: Using undefined variable
+    x = y + 5
+
+    # Error: Division by zero
+    result = 10 / 0
+
+    # Error: Unreachable code
+    print("This code is unreachable.")
+    return result
+
+# Call the function
+oops_program()
