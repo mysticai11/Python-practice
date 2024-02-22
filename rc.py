8afde098707b19e2c0c2d24b1ad69883d4360ca7
Karamjeet singh <149044321+mysticai11@users.noreply.github.com>
2024-02-22 22:26:54 +0530

    Create rc.py

---
+++ rc.py
@@ -0,0 +1,13 @@
+"""Create a recursive function to calculate the factorial of a number."""
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+result = factorial(num)
+print("Factorial of", num, "is", result)
